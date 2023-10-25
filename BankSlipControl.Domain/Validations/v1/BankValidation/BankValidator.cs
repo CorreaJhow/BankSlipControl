4861dca612772c6935e815c108535e5e4965a5e0
Jhonatas Correa <Jhonatasrcorrea@gmail.com>
2023-10-25 17:47:10 -0300

    Alterando configuração do FluentValidation

--- BankSlipControl.Domain/Validations/v1/BankValidation/BankValidator.cs
+++ BankSlipControl.Domain/Validations/v1/BankValidation/BankValidator.cs
@@ -9,7 +9,7 @@ namespace BankSlipControl.Domain.Validations.v1.BankValidation
         {
             RuleFor(p => p.Name).NotEmpty().WithMessage("Name is a required field");
             RuleFor(b => b.Code).NotEmpty().Length(3).Must(BeValidBankCode).WithMessage("The bank code is a mandatory field and must be a sequence of three numeric digits");
-            RuleFor(p => p.InterestPercentage).NotEmpty().WithMessage("InterestPercentage is a required field");
+            RuleFor(p => p.InterestPercentage).GreaterThanOrEqualTo(0).WithMessage("InterestPercentage must be greater than or equal to 0");
         }
 
         private static bool BeValidBankCode(string code)
--- BankSlipControl/Program.cs
+++ BankSlipControl/Program.cs
@@ -1,8 +1,13 @@
+using BankSlipControl.Domain.InputModels.v1.Bank;
+using BankSlipControl.Domain.InputModels.v1.BankSlip;
 using BankSlipControl.Domain.Mappers.v1.BankProfile;
 using BankSlipControl.Domain.Services.v1.BankContract;
 using BankSlipControl.Domain.Services.v1.BankSlipContract;
+using BankSlipControl.Domain.Validations.v1.BankSlipValidation;
+using BankSlipControl.Domain.Validations.v1.BankValidation;
 using BankSlipControl.Infrastructure.ImplementationPersistence.v1;
 using BankSlipControl.Infrastructure.ImplementationPersistence.v1.Implementation;
+using FluentValidation;
 using FluentValidation.AspNetCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -16,10 +21,8 @@ namespace BankSlipControl
 
             // Add services to the container.
 
-            builder.Services.AddControllers().AddFluentValidation(config =>
-            {
-                config.RegisterValidatorsFromAssembly(typeof(Program).Assembly);
-            });
+            builder.Services.AddControllers().AddFluentValidation();
+
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
@@ -30,6 +33,9 @@ namespace BankSlipControl
 
             builder.Services.AddTransient<IBankSlipService, BankSlipService>();
             builder.Services.AddTransient<IBankService, BankService>();
+
+            builder.Services.AddTransient<IValidator<BankInputModel>, BankValidator>();
+            builder.Services.AddTransient<IValidator<BankSlipInputModel>, BankSlipValidator>();
 
             builder.Services.AddAutoMapper(typeof(BankProfile));
 
