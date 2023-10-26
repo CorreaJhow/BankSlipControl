12cebb096b53ca8e629b855b58fd7855c866cd4d
Jhonatas Correa <Jhonatasrcorrea@gmail.com>
2023-10-26 01:45:08 -0300

    Testes e inserindo informações na documentação do swagger.

--- BankSlipControl/Program.cs
+++ BankSlipControl/Program.cs
@@ -15,6 +15,7 @@ using FluentValidation.AspNetCore;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System.Text;
 
 namespace BankSlipControl
@@ -31,7 +32,43 @@ namespace BankSlipControl
 
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Avaliação de Conhecimentos em C#",
+                    Description = "BankSlipControl, application of partial management of banks and Bank Slips.",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Jhonatas R Correa",
+                        Email = "jhonatasrcorrea@correa.com",
+                        Url = new Uri("https://www.linkedin.com/in/jhonatas-r-correa/")
+                    }
+                });
+
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "JWT Authorization header using the Bearer scheme",
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "bearer"
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        Array.Empty<string>()
+                    }
+                });
+            });
 
             builder.Services.AddEntityFrameworkNpgsql()
                 .AddDbContext<ContextDb>(opt => opt.UseNpgsql(
